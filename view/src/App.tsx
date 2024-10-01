8bec631f295ede2923a16ed3607c29a178dcaaa8
Artorias2718 <arturoordonez2718@gmail.com>
2024-10-01 07:59:41 -0600

    Used lazy loading on the Root component to help reduce the app build size

--- view/src/App.tsx
+++ view/src/App.tsx
@@ -1,16 +1,32 @@
-import { ReactElement, useEffect } from 'react'
+import { lazy, ReactElement, useEffect } from 'react'
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import AtlasEarth from "./AtlasEarth/AtlasEarth.tsx";
+import AtlasEarth from "./AtlasEarth/index.tsx";
 import Home from "./AtlasEarth/pages/Home";
 import Timeline from "./AtlasEarth/pages/Timeline";
 import Parcels from "./AtlasEarth/pages/Parcels";
 import Advice from "./AtlasEarth/pages/Advice";
 import FAQ from "./AtlasEarth/pages/FAQ";
 import Resources from "./AtlasEarth/pages/Resources";
-import Root from "./Root/";
 import './App.css'
 import { ErrorBoundary } from "react-error-boundary";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const Root = lazy(() => import('./Root'));
+
+const RouteDict = {
+    AtlasEarth: () => {
+        return (
+            <Route path="atlasearth" element={<AtlasEarth />}>
+                <Route index element={<Home />} />
+                <Route path="timeline" element={<Timeline />} />
+                <Route path="parcels" element={<Parcels />} />
+                <Route path="advice" element={<Advice />} />
+                <Route path="faq" element={<FAQ />} />
+                <Route path="resources" element={<Resources />} />
+            </Route>
+        );
+    }
+};
 
 const queryClient = new QueryClient({
     defaultOptions: {
@@ -19,19 +35,6 @@ const queryClient = new QueryClient({
         }
     }
 });
-
-const RouteDict = {
-    AtlasEarth: () => (
-        <Route path="atlasearth" element={<AtlasEarth />}>
-            <Route index element={<Home />} />
-            <Route path="timeline" element={<Timeline />} />
-            <Route path="parcels" element={<Parcels />} />
-            <Route path="advice" element={<Advice />} />
-            <Route path="faq" element={<FAQ />} />
-            <Route path="resources" element={<Resources />} />
-        </Route>
-    )
-};
 
 const App = (): ReactElement => {
     useEffect(() => {
