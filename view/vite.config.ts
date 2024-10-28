273a40e25527203faea0842d539187cee30330da
Artorias2718 <arturoordonez2718@gmail.com>
2024-10-28 13:36:24 -0600

    Applied manual chunking to reduce bundle sizes

--- view/vite.config.ts
+++ view/vite.config.ts
@@ -1,7 +1,52 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  build: {
+      rollupOptions: {
+          output: {
+              manualChunks: (id) => {
+                  if(id.includes('node_modules')) {
+                      if(id.includes('mui')) {
+                          const componentPath = id
+                              .toString()
+                              .split('node_modules/@mui/')[1];
+
+                          if(componentPath) {
+                              // Example logic to categorize MUI components
+                              if (componentPath.startsWith('material')) {
+                                  // Extract the component name
+                                  const componentName = componentPath.split('/')[1];
+                                  return `mui-${componentName}`; // Create a chunk per component
+                              }
+                              else if (componentPath.startsWith('icons')) {
+                                  return 'mui-icons'; // Bundle all icons together
+                              }
+                          }
+                      }
+
+                      if(id.includes('firebase')) {
+                          return 'firebase';
+                      }
+
+                      return id.toString()
+                          .split('node_modules/')[1]
+                          .split('/')[0]
+                          .toString();
+                  }
+              }
+          }
+      }
+  },
+  plugins: [
+      react(),
+      viteStaticCopy({
+        targets: [{
+          src: 'ads.txt',
+          dest: ''
+        }]
+      })
+  ],
 })
